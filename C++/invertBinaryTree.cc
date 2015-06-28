2ebe6b2c955973c2c116d5178e9710fc34912d97
beimenjun <beimenjun@gmail.com>
2015-06-28 23:18:45 +0800

    format

--- C++/invertBinaryTree.cc
+++ C++/invertBinaryTree.cc
@@ -1,11 +1,11 @@
-class Solution {  
-public:  
-    TreeNode* invertTree(TreeNode* root) {
+class Solution {
+public:
+	TreeNode* invertTree(TreeNode* root) {
 		if (root != NULL) {
     		TreeNode *node = invertTree(root -> left);
 			root -> left = invertTree(root -> right);
 			root -> right = node;
-    	}
+		}
 		return root;
-    }
+	}
 }
