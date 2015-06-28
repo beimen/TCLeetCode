bced421ec2eb8784333ff88ce34ebe50cf6c6541
beimenjun <beimenjun@gmail.com>
2015-06-28 23:16:34 +0800

    format

--- C++/invertBinaryTree.cc
+++ C++/invertBinaryTree.cc
@@ -1,7 +1,7 @@
 class Solution {  
 public:  
     TreeNode* invertTree(TreeNode* root) {
-    	if (root != NULL) {
+		if (root != NULL) {
     		TreeNode *node = invertTree(root -> left);
 			root -> left = invertTree(root -> right);
 			root -> right = node;
