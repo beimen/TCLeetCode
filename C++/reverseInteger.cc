950142902256a63568e820340eaf40e4dd4527dd
beimenjun <beimenjun@gmail.com>
2015-06-29 23:19:00 +0800

    Reverse Integer
    https://leetcode.com/problems/reverse-integer/

---
+++ C++/reverseInteger.cc
@@ -0,0 +1,1 @@
+class Solution {public:	int reverse(int x) {		if (x > 2147483647 || x < -2147483648) {			return 0;		}		long rev = 0;		while(x != 0) {			rev = rev * 10 + x % 10;			x = x / 10;		}		if (rev > 2147483647 || rev < -2147483648) {			rev = 0;		}		return (int)rev;	}};
